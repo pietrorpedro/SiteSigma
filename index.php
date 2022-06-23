edc7fadfa37c7ae3a65ee75ba9abb7cbd22d1b07
pietrorpedro <pietrorpedro@gmail.com>
2022-06-23 11:43:18 -0300

    23/06

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ include_once '_dados.php';
 ?>
 
 <div class="container">
+    <h1>Promo</h1>
     <div class="row m-3" id="cards">
         <?php
         //laço de repeticao, vai exibir os 3 primeiros produtos
@@ -17,7 +18,7 @@ include_once '_dados.php';
         <div class="card-body">
             <h5 class="card-title"><?php echo $produtos[$i]['nome'];?></h5>
             <p class="card-text"><?php echo $produtos[$i]['descricao'];?></p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <a href="produto-detalhe.php?id=<?php echo $i;?>&tipo=promocao" class="btn btn-primary">Comprar</a>
         </div>
         </div>
         <?php
--- produto-detalhe.php
+++ produto-detalhe.php
@@ -1,11 +1,25 @@
 <?php
 // include do footer
-include_once './includes/_dados.php';
 include_once './includes/_head.php';
 include_once './includes/_header.php';
-?>
+include_once '_dados.php';
 
-<h1>Produto Detalhe</h1>
+
+$id = $_GET['id'];
+$tipo = $_GET['tipo'];
+?>
+<div class="center">
+    <img src="<?php echo $produtos[$id]['imagem'];?>">
+    <h1><?php echo $produtos[$id]['nome'];?></h1>
+    <h5><?php echo $produtos[$id]['preco'];?></h5>
+    <p><?php echo $produtos[$id]['descricao'];?></p>
+
+    <div class="row">
+        <div class="col">Voltar</div>
+        <div class="col">Comprar</div>
+    </div>
+</div>
+
 
 <?php
 // include do footer
