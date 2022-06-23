8c86a97326af252839857fe60cd1deec58b150ff
pietrorpedro <pietrorpedro@gmail.com>
2022-06-23 10:15:40 -0300

    Atualizacao

---
+++ _dados.php
@@ -0,0 +1,13 @@
+<?php
+$produtos = array(
+    0 => array('nome' => 'Pietro','preco'=>'Sem preço bb','descricao'=>'Pietro muito legal','imagem'=>'https://pbs.twimg.com/profile_images/1272925818122051585/kEkTKbr2_400x400.jpg'),
+    1 => array('nome' => 'Capivara brava','preco'=>'666','descricao'=>'muito brava','imagem'=>'./content/capivara.jpg'),
+    2 => array('nome' => '','preco'=>'','descricao'=>'','imagem'=>''),
+    3 => array('nome' => '','preco'=>'','descricao'=>'','imagem'=>''),
+    4 => array('nome' => '','preco'=>'','descricao'=>'','imagem'=>''),
+    5 => array('nome' => '','preco'=>'','descricao'=>'','imagem'=>''),
+    6 => array('nome' => '','preco'=>'','descricao'=>'','imagem'=>''),
+    7 => array('nome' => '','preco'=>'','descricao'=>'','imagem'=>''),
+    8 => array('nome' => '','preco'=>'','descricao'=>'','imagem'=>'')
+);
+?>
--- index.php
+++ index.php
@@ -3,9 +3,23 @@
 include_once './includes/_dados.php';
 include_once './includes/_head.php';
 include_once './includes/_header.php';
+include_once '_dados.php';
+for ($i=0; $i <2; $i++){
+    echo $produtos[$i]['nome'].'<br>';
+    echo $produtos[$i]['preco'].'<br';
+    echo $produtos[$i]['imagem'].'<br>';
+    echo '<img src="'.$produtos[$i]['imagem'].'">'.'<br>';
+    echo '<br>';
+}
 ?>
 
 <h1>Home</h1>
+<?php
+echo $produtos[$id]['nome'];
+echo $produtos[$id]['preco'];
+
+?>
+<img src="<?php echo $produtos[$id]['imagem']?>">
 
 <?php
 // include do footer
