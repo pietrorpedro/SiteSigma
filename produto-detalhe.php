5ccca9be663a9d07c91e9417fb59a93cc5250c86
pietrorpedro <pietrorpedro@gmail.com>
2022-08-09 12:15:13 -0300

    Produto detalhe atualizada

--- produto-detalhe.php
+++ produto-detalhe.php
@@ -10,14 +10,20 @@ $res = mysqli_query($conn, $sql);
 $produto = mysqli_fetch_assoc($res);
 ?>
 
-<div class="container mt-5">
+<div class="container mt-2">
     <div class="produto">
-        <div class="produto-imagem">
-            <img src="<?= $produto['Imagem']?>">
+        <div class="produto-img">
+        <img src="<?= $produto['Imagem']?>">
         </div>
-        <div class="produto-content">
-            <h2><?= $produto['Preco']?></h2>
-            
+        <div class="produto-texto">
+        <h2><?= $produto['Nome']?></h2>
+        <p><?= $produto['Preco']?></p>
+        <h4>Descrição</h4>
+        <p><?= $produto['Descricao']?></p>
+
+        <div class="produto-comprar">
+            <a href="" class="btn btn-dark">COMPRAR</a>
+        </div>
         </div>
     </div>
 </div>
